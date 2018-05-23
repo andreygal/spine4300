b010bcdd770ac35f854a6be6b8027c802abf62d3
andreygal <andreygalper@gmail.com>
2018-05-23 04:24:30 -0400

    resolved trf problem

--- pt_extractor.R
+++ pt_extractor.R
@@ -3,7 +3,8 @@ library(RSQLite)
 options(scipen = 999)
 
 diag_codes <- paste0("dx", sprintf("%02d", c(1:25)))
-#Exclusion Criteria 
+
+#Exclusion Criteria ICD-9
 ocd <- 3003
 ssd <- 3008
 eds <- 30750:30759
@@ -12,4 +13,9 @@ sd  <- 30270:30279
 dc  <- c(3120:3129, 31210:31290)
 sad <- c(3050:3059, 30510:30593)
 del <- c(29011, 2903, 2930, 2931, 78009)
+pd  <- 3010:3019
+osm <- c(730:739, 73010:73029)
+trf <- c(setdiff(80000:82900, as.double(levels(interaction(800:829, 1:9))) * 100),
+         as.double(levels(interaction(800:829, 1:9))) * 10)
+
 
