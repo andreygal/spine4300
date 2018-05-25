7ca8354f852cefbb6cf232d0cfe90e967be047c2
andreygal <andreygalper@gmail.com>
2018-05-24 22:12:47 -0400

    Update pt_extractor.R

--- pt_extractor.R
+++ pt_extractor.R
@@ -44,6 +44,14 @@ crp  <- c(03438, 03439)
 exclus_codes <- noquote(c(ocd, ssd, eds, eld, sd, dc, sad, del, pd, osm, trf, 
                           pfr, pcm, ostp, misc, polm, ric, tub, sbm, nrfb, crp))
 
+#MHD group ICD-9 codes 
+sd <- c(78052, 78054, 34701:34709, 3471, 327.23, 327.24,
+        327.26, 30745:30747, 33394, 78052, 78054, 78059)
+dd <- c(29699, 29621:29626, 2962, 2963, 29631:29636, 3004, 29393, 311)
+ad <- c(29384, 3000, 30001:30009, 30022:30029)
+sd <- c(3074, 3083, 3090, 30924, 30928, 3093, 30981, 30989, 3099, 31389)
+mhd_codes <- c(sd, dd, ad, sd)
+
 #query SPARCS for patients
 drv <- dbDriver("SQLite")
 con <- dbConnect(drv, 'new_test.db', flags = SQLITE_RO)
@@ -54,6 +62,14 @@ query <- "SELECT SUBSTR(dischno, 0 ,5) AS year, *, CAST(age AS INTEGER) AS age
           age >= 18 AND
           year BETWEEN '2009' AND '2011';"
 patients <- as.data.table(dbGetQuery(con, query))
+
+#remove patients meeting the exclusion criteria 
+for(patient in patients) { 
+  for(dx_field in diag_fields) {
+    if (!(dx_field %in% exclus_codes)
+  }
+}
+ 
 
 
 
